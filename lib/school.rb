592b59855ff78ace8e13c43247355ccf32728b8d
Brad Schrag <brad.schrag@gmail.com>
2018-11-12 13:48:42 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if self.roster.include?(grade)
+      self.roster[grade] << name
+    else
+      self.roster[grade] = []
+      self.roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    
+end
